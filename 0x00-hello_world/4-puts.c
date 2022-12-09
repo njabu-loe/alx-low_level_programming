288169af399e4dda598756b91b3dddcf5885139f
njabu-loe <njastk5@gmail.com>
2022-12-08 16:58:24 -0800

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Program that prints the phrase "Programming is like building a multi-lingual puzzle" using the puts function
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+
+}
