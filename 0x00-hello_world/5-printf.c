3984cd0c594163950d68a77f6b24147a85a60628
njabu-loe <njastk5@gmail.com>
2022-12-10 21:21:15 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Prints  'with proper grammar, but the outcome is a piece of art,
+ *
  * Return - zero if exited properly, non zero if otherwise
  */
-
-int main(void)
+main(void)
 {
-My code
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
