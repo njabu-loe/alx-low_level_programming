71517e43211c7541b96efb451e5fa279126889f8
njabu-loe <njastk5@gmail.com>
2022-12-13 17:39:28 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point 
+ * main - Prints _putchar
+ *
  * return - Always 0 success
  */
-
 int main(void)
-
 {
 _putchar('_');
 _putchar('p');
@@ -17,5 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return 0;
+return (0);
 }
